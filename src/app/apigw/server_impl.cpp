f0baa4342f161abd7d2f74e58a6e495d44be0035
mloves0824 <mloves0824@163.com>
2018-08-31 10:12:12 +0800

    skip

--- src/app/apigw/server_impl.cpp
+++ src/app/apigw/server_impl.cpp
@@ -15,4 +15,44 @@
 // Author: chenbang (chenbang@antalk.com)
 // Date: $time$
 
+#include "server_impl.h"
 
+namespace antalk {
+namespace apigw {
+
+void LoginServiceImpl::Login(google::protobuf::RpcController* cntl_base,
+    const LoginReq* request,
+    LoginResp* response,
+    google::protobuf::Closure* done) {
+    // This object helps you to call done->Run() in RAII style. If you need
+    // to process the request asynchronously, pass done_guard.release().
+    brpc::ClosureGuard done_guard(done);
+
+    brpc::Controller* cntl =
+        static_cast<brpc::Controller*>(cntl_base);
+
+    // The purpose of following logs is to help you to understand
+    // how clients interact with servers more intuitively. You should 
+    // remove these logs in performance-sensitive servers.
+    LOG(INFO) << "Received request[log_id=" << cntl->log_id()
+        << "] from " << cntl->remote_side()
+        << " to " << cntl->local_side()
+        //<< ": " << request->message()
+        << " (attached=" << cntl->request_attachment() << ")";
+
+    // Fill response.
+    response->set_result_code(antalk::common::REFUSE_REASON_NONE);
+
+    // You can compress the response by setting Controller, but be aware
+    // that compression may be costly, evaluate before turning on.
+    // cntl->set_response_compress_type(brpc::COMPRESS_TYPE_GZIP);
+
+    //if (FLAGS_echo_attachment) {
+    // Set attachment which is wired to network directly instead of
+    // being serialized into protobuf messages.
+    //cntl->response_attachment().append(cntl->request_attachment());
+    //}
+}
+
+}
+}
--- src/app/apigw/server_impl.h
+++ src/app/apigw/server_impl.h
@@ -16,9 +16,8 @@
 // Date: $time$
 
 #include <gflags/gflags.h>
-#include "apigw.pb.h"
 #include <brpc/server.h>
-
+#include "apigw.pb.h"
 
 // Your implementation of im::login::IMLoginService
 // Notice that implementing brpc::Describable grants the ability to put
@@ -30,39 +29,13 @@ class LoginServiceImpl : public LoginService {
 public:
 	LoginServiceImpl() {};
 	virtual ~LoginServiceImpl() {};
-	virtual void Login(google::protobuf::RpcController* cntl_base,
-		const LoginReq* request,
-		LoginResp* response,
-		google::protobuf::Closure* done) {
-		// This object helps you to call done->Run() in RAII style. If you need
-		// to process the request asynchronously, pass done_guard.release().
-//		brpc::ClosureGuard done_guard(done);
-//
-//		brpc::Controller* cntl =
-//			static_cast<brpc::Controller*>(cntl_base);
-//
-//		// The purpose of following logs is to help you to understand
-//		// how clients interact with servers more intuitively. You should 
-//		// remove these logs in performance-sensitive servers.
-//		LOG(INFO) << "Received request[log_id=" << cntl->log_id()
-//			<< "] from " << cntl->remote_side()
-//			<< " to " << cntl->local_side()
-//			//<< ": " << request->message()
-//			<< " (attached=" << cntl->request_attachment() << ")";
-//
-//		// Fill response.
-//		response->set_result_code(im::base::REFUSE_REASON_NONE);
-//
-//		// You can compress the response by setting Controller, but be aware
-//		// that compression may be costly, evaluate before turning on.
-//		// cntl->set_response_compress_type(brpc::COMPRESS_TYPE_GZIP);
-//
-//		//if (FLAGS_echo_attachment) {
-//			// Set attachment which is wired to network directly instead of
-//			// being serialized into protobuf messages.
-//			//cntl->response_attachment().append(cntl->request_attachment());
-//		//}
-	}
+    virtual void Login(google::protobuf::RpcController* cntl_base,
+        const LoginReq* request,
+        LoginResp* response,
+        google::protobuf::Closure* done);
+
+private:
+    antalk::common::ResultType CheckAuth();
 };
 
 }  // namespace example
